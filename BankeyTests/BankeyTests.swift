aef077111d0e677df04bb84150d1f77a99e3e652
Reza <rezafh19@gmail.com>
2024-12-16 16:36:59 +0700

    init

---
+++ BankeyTests/BankeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyTests.swift
+//  BankeyTests
+//
+//  Created by Kompas Digital on 16/12/24.
+//
+
+import Testing
+@testable import Bankey
+
+struct BankeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
